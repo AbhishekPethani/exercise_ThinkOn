2b16e3a6d72640b8bedead207e2bbb5e76e4cf80
Abhishek Pethani <abhishekpethani17@gmail.com>
2024-10-10 16:07:14 -0300

    Created a user model to store the user info.

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}
